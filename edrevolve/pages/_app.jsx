8e71c8778214e2846d8b324454b7b20c1ea4c250
michaeldacanay <michaeldacanayusa@gmail.com>
2023-02-18 23:56:22 -0800

    Add ConvexProvider to _app.jsx

---
+++ edrevolve/pages/_app.jsx
@@ -0,0 +1,16 @@
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import App from 'next/app';
+
+const address = import.meta.env.NEXT_PUBLIC_CONVEX_URL;
+const convex = new ConvexReactClient(address);
+
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <ConvexProvider client={convex}>
+            <Component {...pageProps} />
+        </ConvexProvider>
+    );
+}
+  
+export default MyApp;
