695a9498078064f850f1bdd418f9bfcd9ee07cbc
kentalin <kentalin@usc.edu>
2023-02-19 02:49:37 -0800

    adding images, putting onto homepage

--- edrevolve/app/about/page.tsx
+++ edrevolve/app/about/page.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { useMutation, useQuery } from "../../convex/_generated/react";
+import * as React from 'react';  
+
+
 async function getDataTreehack() {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
   const data = await res.json();
@@ -5,5 +10,118 @@ async function getDataTreehack() {
 }
 
 export default function AboutPage() {
-    return 'About Page'
+  const messages = useQuery("listMessages") || [];
+  
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [school, setSchool] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+  const [addIn, setAddIn] = useState("");
+  const [newMessageText, setNewMessageText] = useState("");
+  const sendMessage = useMutation("sendMessage");
+  
+
+  const [username] = useState(() => "User " + Math.floor(Math.random() * 10000));
+  async function handleSendMessage(event) {
+    event.preventDefault();
+    setNewMessageText("");
+    await sendMessage(newMessageText, username);
+  }
+  
+  return (
+    <main>
+      <h1>Booking Details</h1>
+      <p className="badge">
+        <span>{username}</span>
+      </p>
+
+      <ul>
+        {messages.map(message => (
+          <li key={message._id.toString()}>
+            <span>{message.author}:</span>
+            <span>{message.body}</span>
+            <span>{new Date(message._creationTime).toLocaleTimeString()}</span>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSendMessage}>
+        <input
+          value={newMessageText}
+          onChange={event => setNewMessageText(event.target.value)}
+          placeholder="Write a message…"
+        />
+        <input type="submit" value="Send" disabled={!newMessageText} />
+      </form>
+
+      <div>
+        <ul>
+          <li>
+          <input type="radio" value="Geography" name="subject" /> Geography
+          </li>
+          <li>
+          <input type="radio" value="Science" name="subject" /> Science
+          </li>
+          <li>
+          <input type="radio" value="History" name="subject" /> History
+          </li>
+        </ul>
+      </div>
+
+      <h3>Contact Information</h3>
+
+      <input
+          value={name}
+          onChange={event => setName(event.target.value)}
+          placeholder="Name"
+        />
+        <input
+          value={phoneNumber}
+          onChange={event => setPhoneNumber(event.target.value)}
+          placeholder="Phone Number"
+        />
+        <input
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+          placeholder="Email"
+        />
+        <input
+          value={school}
+          onChange={event => setSchool(event.target.value)}
+          placeholder="School"
+        />
+        <h2>School Address</h2>
+        <input
+          value={street}
+          onChange={event => setStreet(event.target.value)}
+          placeholder="Street"
+        />
+        <input
+          value={city}
+          onChange={event => setCity(event.target.value)}
+          placeholder="City"
+        />
+        <input
+          value={state}
+          onChange={event => setState(event.target.value)}
+          placeholder="State"
+        />
+        <input
+          value={zip}
+          onChange={event => setZip(event.target.value)}
+          placeholder="Zip"
+        />
+          
+        <input
+          value={addIn}
+          onChange={event => setAddIn(event.target.value)}
+          placeholder="Additional Information/Remarks"
+        />
+    
+    </main>
+  );
+
 }
--- edrevolve/app/page.tsx
+++ edrevolve/app/page.tsx
@@ -11,88 +11,47 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <body>
+          <Image
+            src="/1.png"
+            alt="1"
+            className={styles.vercelLogo}
+            width={1540}
+            height={866}
+            priority
+          />
+          
+          
+          <Image
+            src="/2.png"
+            alt="2"
+            className={styles.vercelLogo}
+            width={1540}
+            height={700}
+            priority
+          />
+          <Image
+            src="/3.png"
+            alt="3"
+            className={styles.vercelLogo}
+            width={1540}
+            height={866}
+            priority
+          />
+          <Image
+            src="/4.png"
+            alt="4"
+            className={styles.vercelLogo}
+            width={1540}
+            height={866}
+            priority
+          />
+        </body>
+    
+        <button>BOOK NOW! 
+         
+        </button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel. test
-          </p>
-        </a>
-      </div>
-
-      <Link href="/about">
-        <button>Go to About Page</button>
-      </Link>
     </main>
   )
 }
